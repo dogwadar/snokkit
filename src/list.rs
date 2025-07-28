34e87a81ee6f1e2240ca623bf9f2dcd25fd3fc39
dogwadar <eexepts@gmail.com>
2025-07-28 17:39:30 +0200

    minor, autistic change

--- src/list.rs
+++ src/list.rs
@@ -2,9 +2,8 @@ use pnet::datalink::{self};
 
 pub fn list_interfaces() {
     let interfaces = datalink::interfaces();
-
-    println!("\nInterfaces found: {}\n\n", interfaces.len());
-    for i in interfaces {
-        println!("{i}\n");
-    }
+    println!("\nInterfaces found: {}\n", interfaces.len());
+    interfaces
+        .into_iter()
+        .for_each(|iface| println!("{iface}\n"));
 }
