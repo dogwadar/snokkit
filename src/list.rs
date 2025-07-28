a224b13ce11894572d85fd57b57dba4fd9aba8c1
dogwadar <eexepts@gmail.com>
2025-07-28 17:40:51 +0200

    minor, autistic revert

--- src/list.rs
+++ src/list.rs
@@ -2,8 +2,9 @@ use pnet::datalink::{self};
 
 pub fn list_interfaces() {
     let interfaces = datalink::interfaces();
-    println!("\nInterfaces found: {}\n", interfaces.len());
-    interfaces
-        .into_iter()
-        .for_each(|iface| println!("{iface}\n"));
+
+    println!("\nInterfaces found: {}\n\n", interfaces.len());
+    for i in interfaces {
+        println!("{i}\n");
+    }
 }
